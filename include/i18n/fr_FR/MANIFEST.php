bdc4c11d0983f7c5d4673ec32136b30eef56ac25
Thierry Arnoux <thierry.arnoux@gmx.net>
2015-12-19 20:03:35 +0800

    French language pack
    French language pack

---
+++ include/i18n/fr_FR/MANIFEST.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'Build-Date' => 'Fri, 13 Mar 15 10:14:03 -0500',
+  'Build-Version' => 'v1.9.6',
+  'Language' => 'fr_FR',
+  'Id' => 'lang:fr',
+  'Last-Revision' => '2015-03-13 11:03-0400',
+  'Version' => 142625,
+);
